758c1dcb1dbaffdb6a74f46b207d0b0956932638
wasim-osama <contact.wasimosama@gmail.com>
2024-03-08 16:34:35 +0600

    date dynamic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
+let yearDoc = document.getElementById('year')
 $(function (){
     $('#read-more-btn').click(function (){
         $('#read-more').slideToggle();
-        console.log(12)
     })
 })
+function getYear(){
+
+    let date  =  new Date();
+    let year = '';
+    year = date.getFullYear();
+    yearDoc.innerHTML = year;
+}
+window.onload = () => {
+    getYear();
+}
